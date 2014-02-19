9800dc4412fd813259327664e671a7f9e8db77f0
3rd-Eden <info@3rd-Eden.com>
2014-02-19 19:16:55 +0100

    [test] Make sure that we can stringify the data structure and that it's correct.

--- test/resolve.test.js
+++ test/resolve.test.js
@@ -3,7 +3,18 @@ describe('resolve', function () {
 
   var resolve = require('../resolve')
     , chai = require('chai')
-    , expect = chai.expect;
+    , expect = chai.expect
+    , data;
+
+  this.timeout(20000);
+
+  before(function clean(done) {
+    resolve('primus', function resolved(err, obj) {
+      data = obj;
+
+      done(err);
+    });
+  });
 
   it('exposes as a single function', function () {
     expect(resolve).to.be.a('function');
@@ -11,5 +22,14 @@ describe('resolve', function () {
 
   it('exposes the reducer', function () {
     expect(resolve.reduce).to.be.a('function');
+  });
+
+  it('adds `dependent` on the data structure', function () {
+    expect(data.dependent).to.be.a('object');
+    expect(Object.keys(data.dependent).length).to.be.above(2);
+  });
+
+  it('can JSON.stringify the data structure', function () {
+    JSON.stringify(data);
   });
 });
