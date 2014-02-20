fdb6b54ef65385b6e2d97d5d7b5d9afe66988292
3rd-Eden <info@3rd-Eden.com>
2014-02-20 10:03:02 +0100

    [fix] Fixed typo's and a different URL

--- example/pages/index.js
+++ example/pages/index.js
@@ -3,9 +3,9 @@
 var Page = require('bigpipe').Page;
 
 Page.extend({
-  path: '/:name',         // HTTP route we should respond to.
+  path: '/package/:name', // HTTP route we should respond to.
   view: './index.ejs',    // The base template we need to render.
-  paglets: {              // The pagelets that should be rendered.
+  pagelets: {             // The pagelets that should be rendered.
     package: require('../../')
   }
 }).on(module);
