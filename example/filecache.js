178aedd89df5c921b34189f34224031c7ede118f
3rd-Eden <info@3rd-Eden.com>
2014-02-20 12:06:35 +0100

    [example] Really minimal example of implementing a disk based cache.

---
+++ example/filecache.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var path = require('path')
+  , fs = require('fs');
+
+/**
+ * Really minimal implementation of a data cache sytem. This should not be used
+ * in production it merely serves as an example of caching the data to speedup
+ * the rendering of the pages.
+ *
+ * @constructor
+ * @param {Object} options Configuration.
+ * @api public
+ */
+function Filecache(options) {
+  options = options || {};
+
+  this.directory = options.directory || path.join(__dirname, '/.cache');
+  this.fresh = +options.fresh || 500000;
+}
+
+/**
+ * Get stored data from the disk.
+ *
+ * @param {String} name The name of the module.
+ * @param {Function} fn The callback
+ * @api public
+ */
+Filecache.prototype.get = function get(name, fn) {
+  fs.readFile(path.join(this.directory, '/'+ name +'.json'), 'utf-8', function (err, data) {
+    if (err) return fn(err);
+
+    try { data = JSON.parse(data); }
+    catch (e) { return fn(e); }
+
+    fn(undefined, data);
+  });
+};
+
+/**
+ * Store a new cache file.
+ *
+ * @param {String} name The name of the module.
+ * @param {Function} fn The callback
+ * @api public
+ */
+Filecache.prototype.set = function set(name, data, fn) {
+  try { data = JSON.stringify(data); }
+  catch(e) { return fn(e); }
+
+  fs.writeFile(path.join(this.directory, '/'+ name +'.json'), data, fn);
+};
+
+//
+// Expose the simple cache
+//
+module.exports = Filecache;
--- example/pages/index.js
+++ example/pages/index.js
@@ -1,11 +1,14 @@
 'use strict';
 
-var Page = require('bigpipe').Page;
+var Filecache = require('../filecache')
+  , Page = require('bigpipe').Page;
 
 Page.extend({
   path: '/package/:name', // HTTP route we should respond to.
   view: './index.ejs',    // The base template we need to render.
   pagelets: {             // The pagelets that should be rendered.
-    package: require('../../')
+    package: require('../../').extend({
+      cache: new Filecache()
+    })
   }
 }).on(module);
