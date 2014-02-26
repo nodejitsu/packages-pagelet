f06f065bbd8d0e752b5f9ae3dfc1a3ce167a2ff9
3rd-Eden <info@3rd-Eden.com>
2014-02-26 17:11:26 +0100

    [example] Added example usage for authorized githulks

--- example/pages/index.js
+++ example/pages/index.js
@@ -1,14 +1,18 @@
 'use strict';
 
 var Filecache = require('../filecache')
-  , Page = require('bigpipe').Page;
+  , Page = require('bigpipe').Page
+  , GitHulk = require('githulk');
 
 Page.extend({
   path: '/package/:name', // HTTP route we should respond to.
   view: './index.ejs',    // The base template we need to render.
   pagelets: {             // The pagelets that should be rendered.
     package: require('../../').extend({
-      cache: new Filecache()
+      cache: new Filecache(),
+      githulk: new GitHulk({
+        token: process.env.GITHUB_TOKEN
+      })
     })
   }
 }).on(module);
