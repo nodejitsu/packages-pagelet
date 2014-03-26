fbad062ab51df1e46f75fd9c425791b12b0e1180
Swaagie <info@martijnswaagman.nl>
2014-03-26 15:03:50 +0100

    [tests] update and fix

--- test/pagelet.test.js
+++ test/pagelet.test.js
@@ -3,6 +3,7 @@ describe('pagelet', function () {
 
   var Pagelet = require('../')
     , chai = require('chai')
+    , major = require('../package.json').version.slice(0, 1)
     , expect = chai.expect;
 
   it('exposes the module as single function', function () {
@@ -13,7 +14,13 @@ describe('pagelet', function () {
     it('should prefix the module with our major version number', function () {
       var pagelet = new Pagelet();
 
-      expect(pagelet.key('foo')).to.equal('v0:foo');
+      expect(pagelet.key('foo', '1.0.0')).to.equal('v'+ major +':foo@1.0.0');
+    });
+
+    it('should append package@version', function () {
+      var pagelet = new Pagelet();
+
+      expect(pagelet.key('foo', '2.0.0')).to.equal('v'+ major +':foo@2.0.0');
     });
   });
 });
--- test/resolve.test.js
+++ test/resolve.test.js
@@ -24,9 +24,35 @@ describe('resolve', function () {
     expect(resolve.reduce).to.be.a('function');
   });
 
-  it('adds `dependent` on the data structure', function () {
-    expect(data.dependent).to.be.a('object');
-    expect(Object.keys(data.dependent).length).to.be.above(2);
+  it('adds sorted `shrinkwrap` on the data structure', function () {
+    expect(data.shrinkwrap).to.be.an('array');
+    expect(data.shrinkwrap.length).to.be.above(2);
+    expect(data.shrinkwrap[0].main).to.be.equal(1);
+
+    expect(data.shrinkwrap.dependencies).to.equal(undefined);
+    expect(data.shrinkwrap.dependent).to.equal(undefined);
+    expect(data.shrinkwrap.parent).to.equal(undefined);
+  });
+
+  it('adds readme on the data structure', function () {
+    expect(data.readme).to.be.a('string');
+    expect(data.readme).to.include('<h1 id="primus">Primus</h1>');
+  });
+
+  it('adds package on the data structure', function () {
+    expect(data.package).to.be.an('object');
+    expect(data.package.releases).to.be.an('object');
+    expect(data.package.time).to.be.an('object');
+
+    expect(data.package.readmeFilename).to.equal(undefined);
+    expect(data.package.versions).to.equal(undefined);
+    expect(data.package.readme).to.equal(undefined);
+  });
+
+  it('adds github on the data structure', function () {
+    expect(data.github).to.be.an('object');
+    expect(data.github.name).to.equal('primus');
+    expect(data.github.full_name).to.equal('primus/primus');
   });
 
   it('can JSON.stringify the data structure', function () {
