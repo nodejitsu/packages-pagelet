42cd9d9e80547b8dac0105ae23d14cc9bc778aa6
Swaagie <info@martijnswaagman.nl>
2014-03-27 13:52:45 +0100

    [ux] allow show less

--- package.js
+++ package.js
@@ -13,13 +13,16 @@ pipe.once('package::initialise', function (data, pagelet) {
   pagelet.on('click', '.details .show-all', function click(e) {
     e.preventDefault();
 
-    var parent = $(this).parents('.fourcol');
+    var element = $(this)
+      , text = element.text()
+      , state = element.data('state')
+      , parent = element.parents('.fourcol');
 
     //
     // Show the rows that were hidden and remove the table foot as we're already
     // showing all the fields now.
     //
-    parent.find('.gone').fadeIn();
-    $(this).remove();
+    parent.find('.gone')[state ? 'fadeOut' : 'fadeIn']();
+    element.text(element.data('text')).data('text', text).data('state', !state);
   });
 });
